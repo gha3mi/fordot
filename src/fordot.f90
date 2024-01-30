100025f2fddf8ad9894172e02427a7a3fbb5d739
gha3mi <36126354+gha3mi@users.noreply.github.com>
2024-01-30 22:29:52 +0000

    Deploying to gh-pages from @ gha3mi/fordot@b6aed219fd3f8593aa6564604d65bdfd2e2f7f30 🚀

--- src/fordot.f90
+++ src/fordot.f90
@@ -20,9 +20,13 @@ contains
    pure function dot_R0R1R1_rel(u,v,option) result(a)
       real(rk),     intent(in), contiguous :: u(:)
       real(rk),     intent(in), contiguous :: v(:)
-      character(*), intent(in)             :: option
+      character(*), intent(in), optional   :: option
       real(rk)                             :: a
-      a = dot_opts(u, v, option)
+      if (present(option)) then
+         a = dot_opts(u, v, option)
+      else
+         a = dot_opts(u, v, option='m2')
+      end if
    end function dot_R0R1R1_rel
 
 
@@ -31,31 +35,40 @@ contains
    pure function dot_R0R1R1_rel_block(u,v,option,nblock) result(a)
       real(rk),     intent(in), contiguous :: u(:)
       real(rk),     intent(in), contiguous :: v(:)
-      character(*), intent(in)             :: option
+      character(*), intent(in), optional   :: option
       integer,      intent(in)             :: nblock
       real(rk)                             :: a
-      integer                              :: im
+      integer                              :: im, se, ee
       integer                              :: block_size(nblock), start_elem(nblock), end_elem(nblock)
 
       call compute_block_ranges(size(u), nblock, block_size, start_elem, end_elem)
       a = 0.0_rk
+#if defined(USE_DO_CONCURRENT)
+      do concurrent (im = 1: nblock) reduce(+:a)
+         se = start_elem(im)
+         ee = end_elem(im)
+         a = a + dot_product(u(se:ee),v(se:ee),option)
+    end do
+#else
       do im = 1, nblock
-         a = a + dot_product(u(start_elem(im):end_elem(im)),v(start_elem(im):end_elem(im)),option)
+         se = start_elem(im)
+         ee = end_elem(im)
+         a = a + dot_product(u(se:ee),v(se:ee),option)
       end do
-      
+#endif
    end function dot_R0R1R1_rel_block
 
 
 
    !> author: Seyed Ali Ghasemi
    impure function dot_R0R1R1_rel_coarray(u,v,option,coarray) result(a)
-      real(rk),     intent(in) :: u(:)
-      real(rk),     intent(in) :: v(:)
-      character(*), intent(in)             :: option
+      real(rk),     intent(in), contiguous :: u(:)
+      real(rk),     intent(in), contiguous :: v(:)
+      character(*), intent(in), optional   :: option
       real(rk)                             :: a
       logical,      intent(in)             :: coarray
 #if defined(USE_COARRAY)
-      integer               :: i, im, nimg, m
+      integer               :: i, im, nimg, m, se, ee
       integer               :: block_size(num_images()), start_elem(num_images()), end_elem(num_images())
       real(rk), allocatable :: a_block[:], u_block(:)[:], v_block(:)[:]
 
@@ -64,18 +77,20 @@ contains
       m    = size(u)
       call compute_block_ranges(size(u), nimg, block_size, start_elem, end_elem)
       allocate(u_block(block_size(im))[*], v_block(block_size(im))[*], a_block[*])
-      u_block(:)[im] = u(start_elem(im):end_elem(im))
-      v_block(:)[im] = v(start_elem(im):end_elem(im))
-      a_block[im] = dot_opts(u_block(:)[im],v_block(:)[im],option)
-      ! call co_sum(a_block, result_image=1)
-      ! a = a_block[1]
-      sync all
-      if (im == 1) then
-         a = 0.0_rk
-         do i = 1, nimg
-            a = a + a_block[i]
-         end do
-      end if
+      se = start_elem(im)
+      ee = end_elem(im)
+      u_block(:)[im] = u(se:ee)
+      v_block(:)[im] = v(se:ee)
+      a_block[im] = dot_product(u_block(:)[im],v_block(:)[im],option)
+      call co_sum(a_block, result_image=1)
+      a = a_block[1]
+      ! sync all
+      ! if (im == 1) then
+      !    a = 0.0_rk
+      !    do i = 1, nimg
+      !       a = a + a_block[i]
+      !    end do
+      ! end if
 #else
       a = dot_product(u, v, option)
 #endif
